21320966d9229fa0e2667bfe82e006663446ba74
fair-laptop-5201 <jesse.griffin@gmail.com>
2020-01-21 02:01:51 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,39 @@
-def addition(num1, num2)
+num1 = 4
+num2 = 10
+num = 25
 
+def addition(num1, num2)
+ puts(num2+num1)
 end
 
 def subtraction(num1, num2)
-
+  puts(num2-num1)
 end
 
 def division(num1, num2)
-
+  puts(num2/num1)
 end
 
 def multiplication(num1, num2)
-
+  puts(num1*num2)
 end
 
 def modulo(num1, num2)
-
+  puts(num2%num1)
 end
 
 def square_root(num)
-
+  puts Math.sqrt(num)
 end
+
+addition
+
+subtraction
+
+multiplication
+
+division
+
+modulo
+
+square_root
