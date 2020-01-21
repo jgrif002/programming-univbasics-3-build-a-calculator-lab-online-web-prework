65ea051dda1caaf9a0c7c4246a12dc22b1fd2c68
fair-laptop-5201 <jesse.griffin@gmail.com>
2020-01-21 01:34:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,15 +3,15 @@ num2 = 10
 num = 25
 
 def addition(num1, num2)
- sum(num1,num2)
+ puts(num2+num1)
 end
 
 def subtraction(num1, num2)
-  dif(num1,num2)
+  puts(num2-num1)
 end
 
 def division(num1, num2)
-
+  puts(num2/num1)
 end
 
 def multiplication(num1, num2)
@@ -19,7 +19,7 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  puts (num2/num1)
+  
 end
 
 def square_root(num)
